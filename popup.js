3fcf0467a81abe274dc0327ef563619fa7c169fe
Andrey <andreshatuno@gmail.com>
2022-06-06 21:28:15 +0500

    delete input

--- popup.js
+++ popup.js
@@ -1,17 +1,18 @@
-let snow = document.getElementById("snow");
+let addLink = document.getElementById("addLink");
 let clearAll = document.getElementById("clearAll");
+let linksList = document.getElementById("linksList");
 
-snow.addEventListener("click", async () => {
+addLink.addEventListener("click", async () => {
 	// получаем доступ к активной вкладке
 	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-	let mylink = document.getElementById("mylink").value;
+	//let mylink = document.getElementById("mylink").value;
 	// выполняем скрипт
 	chrome.scripting.executeScript({
 		// скрипт будет выполняться во вкладке, которую нашли на предыдущем этапе
 		target: { tabId: tab.id },
 		// вызываем функцию, в которой лежит запуск снежинок
 		function: addLinkToStorage,
-		args: [mylink]
+		args: [tab.url]
 	});
 });
 
@@ -27,6 +28,16 @@ clearAll.addEventListener("click", async () => {
 	 });
 });
 
+linksList.addEventListener("click", async () => {
+	chrome.tabs.update({url: "linksList.html"});
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+	chrome.storage.local.get({pearsurls: []}, function (result) {
+		pearsurls = result.pearsurls;
+		document.getElementById("lst").innerHTML = pearsurls.join(" <br> ");
+	});
+});
 
 function clearAllStorage(){
 	chrome.storage.local.clear(function() {
@@ -39,14 +50,17 @@ function clearAllStorage(){
 
 function addLinkToStorage(mylink) {
 	if(mylink.length > 0){
-		mylink = mylink.replaceAll('http://', '')
-		mylink = mylink.replaceAll('https://', '')
-		console.log(mylink)
+		//mylink = mylink.replaceAll('http://', '')
+		//mylink = mylink.replaceAll('https://', '')
+		//console.log(mylink)
+		var parser = document.createElement('a');
+		parser.href = mylink
+		//console.log(tab.url);
 		chrome.storage.local.get({pearsurls: []}, function (result) {
 			pearsurls = result.pearsurls;
-			console.log(pearsurls);
-			pearsurls.push(mylink)
-			console.log(pearsurls);
+			//console.log(pearsurls);
+			pearsurls.push(parser.hostname)
+			//console.log(pearsurls);
 			//console.log(pearsurls.length)
 			chrome.storage.local.set({'pearsurls': pearsurls});
 		});
